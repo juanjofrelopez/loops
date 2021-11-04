75ddbcd7bc9a4649d1f1d6899d86489cf27228f4
juan <juanjofrelopez@gmail.com>
2021-11-03 21:53:19 -0300

    second commit

--- src/main.rs
+++ src/main.rs
@@ -29,4 +29,34 @@ fn main() {
         }
     }; // hay que poner un punto y coma
     println!("result was: {}",result);
+
+    // WHILE LOOP
+    let mut number =3;
+    while number != 0 {
+        println!("{}",number);
+        number-=1;
+    }
+
+    // FOR LOOP: iterating over elements of a collection
+        // for example an array
+    let a = [9,8,7,6,5,4];
+    for every_element in a {
+        println!("{}",every_element);
+    }
+        // short version of a countdown
+        // using Range and rev()
+    println!("COUNTDOWN");
+    for number in (1..4).rev() {
+        println!("{}",number);  
+    }
 }
+
+/*
+FOR loops increase the efficiency and safety. Also we eliminated
+the chance of bugs because we are not managing index like
+we'd do on C-style code.
+
+When we want to run some code a certain number of times 
+we use a for loop with a type called Range
+
+*/
